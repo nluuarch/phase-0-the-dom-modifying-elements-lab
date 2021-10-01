6fa2f93cf7806ac70be78b92be555aab569a2869
nluuarch@gmail.com <nluuarch@gmail.com>
2021-09-30 21:14:31 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const ul = document.createElement("ul");
 
-for (let i = 0; i < 3; i++) {
-  const li = document.createElement("li");
-  li.textContent = (i + 1).toString();
-  ul.append(li);
-}
+const ul = document.getElementsByTagName("main")[0];
+ul.remove()
 
-element.append(ul);
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+newHeader.textContent = "PANTZ is the champion";
+
+
